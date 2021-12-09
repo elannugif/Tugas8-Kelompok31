07bdc5b9de2e0b6536e40eca846fe69415fd81a0
unknown <elannugiferdiyanto@gmail.com>
2021-12-09 15:14:29 +0700

    first commit

---
+++ app/src/main/java/com/developeralamin/mywallpaper/ApiInterface.java
@@ -0,0 +1,31 @@
+package com.developeralamin.mywallpaper;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+import static com.developeralamin.mywallpaper.ApiUtilities.API;
+
+public interface ApiInterface {
+
+
+    String BASE_URL = "https://api.pexels.com/v1/";
+
+    @Headers("Authorization: "+API)
+    @GET("curated")
+    Call<SearchModel> getImage(
+            @Query("page") int page,
+            @Query("per_page") int per_page
+    );
+
+
+    @Headers("Authorization: "+API)
+    @GET("search")
+    Call<SearchModel> getSearchtImage(
+            @Query("query") String query,
+            @Query("query") int page,
+            @Query("per_page") int per_page
+    );
+}
---
+++ app/src/main/java/com/developeralamin/mywallpaper/ImageModel.java
@@ -0,0 +1,19 @@
+package com.developeralamin.mywallpaper;
+
+public class ImageModel {
+
+    private UrlModel src;
+
+    public UrlModel getSrc() {
+        return src;
+    }
+
+    public void setSrc(UrlModel src) {
+        this.src = src;
+    }
+
+    public ImageModel(UrlModel src) {
+        this.src = src;
+    }
+
+}
